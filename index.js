c494018764109e4421586fd4c1c2f1903d5286ef
bilel0509 <billel.boulahia456@gmail.com>
2024-07-20 04:02:59 +0100

    added html

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    databaseURL:'https://winrahtrain-default-rtdb.europe-west1.firebasedatabase.app/',
 };
 
 

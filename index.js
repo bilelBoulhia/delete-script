d4fe877348d1afae985512040f7c6fabee9c55d6
bilel0509 <billel.boulahia456@gmail.com>
2024-07-20 03:03:00 +0100

    upadte

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const { initializeApp } = require("firebase/app");
 const { getDatabase, remove, ref } = require("firebase/database");
 
-// Firebase configuration
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDCJn2KtHEeHNIdQMSLrcNsheQZIBMiIck",
-    authDomain: "winrahtrain.firebaseapp.com",
-    projectId: "winrahtrain",
-    storageBucket: "winrahtrain.appspot.com",
-    messagingSenderId: "109651923705",
-    appId: "1:109651923705:web:2de5f243acaeeaa7204828",
-    databaseURL: "https://winrahtrain-default-rtdb.europe-west1.firebasedatabase.app/",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
-// Initialize Firebase
+
 const fb_app = initializeApp(firebaseConfig);
 const db = getDatabase(fb_app);
 const reference = 'reports/';
 
-// Function to remove the data
+
 const removeData = () => {
     remove(ref(db, reference))
         .then(() => console.log('Data removed successfully'))
